1ddf96353dc73230224d75f196417115d21c018c
zhangjiawei <349071347@qq.com>
2016-06-12 11:43:26 +0800

    add RandomAccessFile demo

--- src/main/java/online/babylove/www/io/README.md
+++ src/main/java/online/babylove/www/io/README.md
@@ -1,4 +1,5 @@
 #IO
+* 在硬盘上的文件是byte byte byte存储的，是数据的集合
 
 ##编码的问题
 * 编码是什么？
@@ -13,3 +14,15 @@
 ##遍历目录
 * [运行入口](https://github.com/l81893521/java-example/blob/master/src/main/java/online/babylove/www/io/demo2/FileUtilTest.java)
 * [查看代码](https://github.com/l81893521/java-example/blob/master/src/main/java/online/babylove/www/io/demo2/FileUtils.java)
+
+##RandomAccessFile
+* Java提供的对文件内容的访问，既可以读文件，也可以写文件
+* 支持随机访问文件，可以访问文件的任意位置
+* [查看代码](https://github.com/l81893521/java-example/blob/master/src/main/java/online/babylove/www/io/demo3/RafDemo.java)
+
+#字节流
+* InputStream抽象了应用程序读取数据的方式
+* OutStream抽象了应用程序写出数据的方式
+* EOF(end of file) = End 读到-1就读到结尾
+* [读取指定文件内容，按照16进制输出到控制台,运行入口(单字节读取)](https://github.com/l81893521/java-example/blob/master/src/main/java/online/babylove/www/io/demo4/IOUtilTest1.java)
+* [读取指定文件内容，按照16进制输出到控制台,运行入口(批量字节读取)](https://github.com/l81893521/java-example/blob/master/src/main/java/online/babylove/www/io/demo4/IOUtilTest2.java)
---
+++ src/main/java/online/babylove/www/io/demo3/RafDemo.java
@@ -0,0 +1,64 @@
+package online.babylove.www.io.demo3;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
+
+
+public class RafDemo {
+	
+	public static void main(String[] args) throws IOException{
+		File demo = new File("demo");
+		if(!demo.exists()){
+			demo.mkdirs();
+		}
+		
+		File file = new File(demo,"raf.dat");
+		if(!file.exists()){
+			file.createNewFile();
+		}
+		//有两种模式"rw"(读写), "r"只读
+		RandomAccessFile raf = new RandomAccessFile(file, "rw");
+		//指针的位置（文件指针），打开文件时指针在开头 pointer = 0;
+		System.out.println(raf.getFilePointer());
+		
+		raf.write('A');//只写了一个字节(char是2个字节),部分字符后八位也能直接表示
+		System.out.println(raf.getFilePointer());
+		
+		raf.write('B');
+		
+		int i = 0x7fffffff;
+		//用write方法每次只能写一个字节，如果要把i写进去就得写4次
+		raf.write(i >>> 24);//高八位
+		raf.write(i >>> 16);//
+		raf.write(i >>> 8);//
+		raf.write(i);//
+		System.out.println(raf.getFilePointer());
+		
+		//也可以直接写int
+		raf.writeInt(i);
+		
+		String s = "中";
+		byte utf8[] = s.getBytes("utf-8");
+		raf.write(utf8);
+		System.out.println(raf.length());
+		
+		//读文件，必须把指针移到头部
+		raf.seek(0);
+//		raf.read()//只读一个字节
+		//一次性读取,把文件中的内容都读到字节数组中
+		byte buf[] = new byte[(int)raf.length()];
+		raf.read(buf);
+		System.out.println(Arrays.toString(buf));
+		
+		String s1 = new String(buf,"");
+		System.out.println(s1);
+		
+		for (byte b : buf) {
+			System.out.print(Integer.toHexString(b & 0xff) + " ");
+		}
+		//文件读写完成以后一定要关闭（官方说明）
+		raf.close();
+	}
+}
